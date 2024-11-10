db5088a265b48cb1df157c3399761776c4b81e32
mihaivaidos <mihai@vaidos.com>
2024-11-10 14:09:36 +0200

    Methods: viewActiveLoans, addBookToCategory, viewAllBooksInCategory and viewAllCategories in Controller.

--- src/LibraryController/LibraryController.java
+++ src/LibraryController/LibraryController.java
@@ -35,4 +35,38 @@ public class LibraryController {
         libraryService.returnBook(loanID);
         System.out.println("Book returned successfully with Loan ID: " + loanID);
     }
+
+    public void viewActiveLoans(int memberID) {
+        List<Loan> loans = libraryService.getActiveLoansForMember(memberID);
+        System.out.println("Active loans for member ID: " + memberID);
+        for(Loan loan : loans) {
+            System.out.println("ID: " + loan.getID() + ", Loan date: " + loan.getLoanDate() + ", Due date: "
+                    + loan.getDueDate() + ", Return date: " + loan.getReturnDate() + ", Status: " + loan.getStatus()
+                    + ", Book: " + loan.getBook().getBookName() + ", Book author: " + loan.getBook().getAuthor().getName());
+        }
+    }
+
+    public void addBookToCategory(int bookID, int categoryID) {
+        libraryService.addBookToCategory(bookID, categoryID);
+        System.out.println("Book added to category successfully.");
+    }
+
+    public void viewAllBooksInCategory(int categoryID) {
+        System.out.println("Available Books in category: " + categoryID);
+        for (Book book : libraryService.getAllBooksInCategory(categoryID)) {
+            String status = book.isAvailable() ? "Available" : "Borrowed";
+            System.out.println("ID: " + book.getID() + ", Title: " + book.getBookName() + ", Author: " + book.getAuthor().getName()
+                    + ", Status: " + status + ", Publisher: " + book.getPublisher().getName() + ", Category: " + book.getCategory().getCategoryName());
+        }
+    }
+
+    public void viewAllCategories() {
+        System.out.println("Categories:");
+        List<Category> categories = libraryService.getAllCategories();
+        for (Category category : categories) {
+            System.out.println("ID: " + category.getID() + ", Name: " + category.getCategoryName()
+                    + ", Description: " + category.getDescription());
+        }
+    }
+
 }
