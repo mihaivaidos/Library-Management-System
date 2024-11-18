659518242e27e76209700470fa820d6fb7a6c32e
SimonSaraMaria <sara.simon@stud.ubbcluj.ro>
2024-11-18 13:33:51 +0200

    Added FileRepository

---
+++ src/LibraryRepository/FileRepository.java
@@ -0,0 +1,100 @@
+package LibraryRepository;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.function.Consumer;
+import LibraryModel.HasID;
+
+/**
+ * A repository implementation that stores data in a file.
+ *
+ * @param <T> The type of objects stored in the repository, which must implement HasId.
+ */
+
+public class FileRepository<T extends HasID> implements IRepository<T> {
+
+    private final String filePath;
+
+    /**
+     * Constructs a new FileRepository with the specified file path.
+     *
+     * @param filePath The path to the file where data will be stored.
+     */
+
+    public FileRepository(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public void add(T obj) {
+
+    }
+
+    @Override
+    public T get(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(T obj) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<T> getAll() {
+        return List.of();
+    }
+
+    /**
+     * Performs an operation on the data stored in the file.
+     *
+     * @param function The function to apply to the data.
+     */
+
+    private void doInFile(Consumer<Map<Integer, T>> function) {
+        Map<Integer, T> data = readDataFromFile();
+        function.accept(data);
+        writeDataToFile(data);
+    }
+
+    /**
+     * Reads the data from the file.
+     *
+     * @return The data stored in the file, or an empty map if the file is empty or does not exist.
+     */
+
+    private Map<Integer, T> readDataFromFile() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
+            return (Map<Integer, T>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            return new HashMap<>();
+        }
+    }
+
+    /**
+     * Writes the data to the file.
+     *
+     * @param data The data to write to the file.
+     */
+
+    private void writeDataToFile(Map<Integer, T> data) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            oos.writeObject(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
